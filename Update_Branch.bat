5ab81acc82ace4ee525b83929df7e6c818b5588c
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2024-09-09 14:57:07 +0800

    Update build tools

--- Update_Branch.bat
+++ Update_Branch.bat
@@ -14,20 +14,24 @@ if %errorlevel%==0 (
     echo Branch '%branch%' exists locally, checking it out...
     git checkout %branch%
     goto UPDATE
+) ELSE (
+    git ls-remote --heads origin %BRANCH% >nul 2>&1
+
+    if %errorlevel% NEQ 0 (
+        echo Invalid branch name. Please try again.
+        goto INPUT
+    )
+
+    git checkout -b %BRANCH% origin/%BRANCH%
 )
 
-git ls-remote --heads origin %BRANCH% >nul 2>&1
 
-if %errorlevel% NEQ 0 (
-    echo Invalid branch name. Please try again.
-    goto INPUT
-)
 
 :UPDATE
 
 echo Updating current repository on branch %BRANCH%...
 
-git checkout -b %BRANCH% origin/%BRANCH%
+
 
 git pull https://git.sg.digipen.edu/projects/team_little_match_f24 %BRANCH%
 
