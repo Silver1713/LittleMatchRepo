4495c7b4a21f6fab6e0ec88a9c443cc08378db32
Edwin <edwinzirui.lee@digipen.edu>
2024-10-03 21:40:50 +0800

    vec4 halfway done

--- Sage/CoreComponents/SageMath_Lib/src/Vector4.cpp
+++ Sage/CoreComponents/SageMath_Lib/src/Vector4.cpp
@@ -1,9 +1,32 @@
+﻿/* Start Header ************************************************************************/
+/*!
+\file		Vector4.cpp
+\title		Memory's Flame
+\author		Edwin Lee Zirui, edwinzirui.lee, 2301299 (100%)
+\par		edwinzirui.lee@digipen.edu
+\date		13 September 2024
+\brief		Vector4 class and class operation defination
+
+			All content � 2024 DigiPen Institute of Technology Singapore. All rights reserved.
+*/
+/* End Header **************************************************************************/
+
 #include "Vector4.h"
 
 #include <cmath>
 
 namespace ToastBox
 {
+	/*!*****************************************************************************
+	  \brief
+	   Copy constructor for Vec4
+
+	  \param pVec0
+	   Vector to be copied
+
+	  \return
+	   New Vec4 object as a copy of pVec0
+	*******************************************************************************/
 	Vec4::Vec4(const Vec4& pVec0)
 	{
 		x = pVec0.x;
@@ -12,8 +35,22 @@ namespace ToastBox
 		w = pVec0.w;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Constructor with individual component values
 
+	  \param x
+	   X component of the vector
+	  \param y
+	   Y component of the vector
+	  \param z
+	   Z component of the vector
+	  \param w
+	   W component of the vector
 
+	  \return
+	   Initialized Vec4 object with specified components
+	*******************************************************************************/
 	Vec4::Vec4(float const& x, float const& y, float const& z, float const& w)
 	{
 		this->x = x;
@@ -22,6 +59,16 @@ namespace ToastBox
 		this->w = w;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Scalar multiplication assignment operator
+
+	  \param value
+	   Scalar to multiply with
+
+	  \return
+	   Reference to this vector after multiplication
+	*******************************************************************************/
 	Vec4& Vec4::operator*=(const float value)
 	{
 		x *= value;
@@ -31,6 +78,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Vector addition assignment operator
+
+	  \param pVec0
+	   Vector to add
+
+	  \return
+	   Reference to this vector after addition
+	*******************************************************************************/
 	Vec4& Vec4::operator+=(const Vec4& pVec0)
 	{
 		x += pVec0.x;
@@ -40,6 +97,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Cross product assignment operator
+
+	  \param pVec0
+	   Vector to cross with
+
+	  \return
+	   Reference to this vector after cross product
+	*******************************************************************************/
 	Vec4& Vec4::operator%=(const Vec4& pVec0)
 	{
 		// Cross product
@@ -55,6 +122,13 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Negation operator
+
+	  \return
+	   Negated vector
+	*******************************************************************************/
 	Vec4& Vec4::operator-()
 	{
 		x = -x;
@@ -64,6 +138,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Vector subtraction assignment operator
+
+	  \param pVec0
+	   Vector to subtract
+
+	  \return
+	   Reference to this vector after subtraction
+	*******************************************************************************/
 	Vec4& Vec4::operator-=(const Vec4& pVec0)
 	{
 		x -= pVec0.x;
@@ -73,7 +157,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Vector division assignment operator
 
+	  \param pVec0
+	   Vector to divide by
+
+	  \return
+	   Reference to this vector after division
+	*******************************************************************************/
 	Vec4& Vec4::operator/=(const Vec4& pVec0)
 	{
 		x /= pVec0.x;
@@ -83,6 +176,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Scalar division assignment operator
+
+	  \param value
+	   Scalar to divide by
+
+	  \return
+	   Reference to this vector after division
+	*******************************************************************************/
 	Vec4& Vec4::operator/=(const float value)
 	{
 		x /= value;
@@ -92,6 +195,16 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Assignment operator
+
+	  \param pVec0
+	   Vector to assign from
+
+	  \return
+	   Reference to this vector after assignment
+	*******************************************************************************/
 	Vec4& Vec4::operator=(const Vec4& pVec0)
 	{
 		x = pVec0.x;
@@ -101,38 +214,74 @@ namespace ToastBox
 		return *this;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Indexed access operator (mutable)
 
+	  \param index
+	   Index of the component to access
+
+	  \return
+	   Reference to the component at the specified index
+	*******************************************************************************/
 	float& Vec4::operator[](int index)
 	{
 		return (&x)[index];
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Indexed access operator (const)
+
+	  \param index
+	   Index of the component to access
+
+	  \return
+	   Value of the component at the specified index
+	*******************************************************************************/
 	float Vec4::operator[](int index) const
 	{
 		return (&x)[index];
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Calculate the magnitude of the vector
 
-
-	float Vec4::magnitude() const
+	  \return
+	   Magnitude of the vector
+	*******************************************************************************/
+	float Vec4::Magnitude() const
 	{
 		return sqrt(x * x + y * y + z * z + w * w);
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Calculate the squared magnitude of the vector
 
-	float Vec4::magnitudeSquared() const
+	  \return
+	   Squared magnitude of the vector
+	*******************************************************************************/
+	float Vec4::Magnitude_Squared() const
 	{
 		return x * x + y * y + z * z + w * w;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Normalize a vector
 
-	void Vec4::normalize(Vec4& pResult, const Vec4& pVec0)
+	  \param pResult
+	   Vector to store the normalized result
+	  \param pVec0
+	   Vector to be normalized
+	*******************************************************************************/
+	void Vec4::Normalize(Vec4& pResult, const Vec4& pVec0)
 	{
 		const Vec4 result{ pVec0 };
 
-		pResult = result / result.magnitude();
-
-
+		pResult = result / result.Magnitude();
 	}
 
 
@@ -212,26 +361,20 @@ namespace ToastBox
 		return res;
 	}
 
-	float* Vec4::data()
+	/*!*****************************************************************************
+	  \brief
+	   Get a pointer to the vector's data
+
+	  \return
+	   Pointer to the first component of the vector
+	*******************************************************************************/
+	float* Vec4::Data()
 	{
 		return &x;
 	}
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
