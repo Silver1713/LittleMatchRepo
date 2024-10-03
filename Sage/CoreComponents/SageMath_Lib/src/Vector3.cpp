5f6526afc58b71c6b838d932612a40245b95b86e
Edwin <edwinzirui.lee@digipen.edu>
2024-10-03 21:22:50 +0800

    edited vector 3 documentations and code conventions

--- Sage/CoreComponents/SageMath_Lib/src/Vector3.cpp
+++ Sage/CoreComponents/SageMath_Lib/src/Vector3.cpp
@@ -1,14 +1,15 @@
-/******************************************************************************
-File name	 : Vector3.cpp
-Project		 : Memory's Flame
-Author       : Edwin Lee Zirui
-E-mail		 : edwinzirui.lee@digipen.edu
-Data		 : 09/09/2024
-Brief		 : Vector3 class and class operation defination
-*
-* \copyright	All content 2024 DigiPen Institute of Technology Singapore. All
-				rights reserved.
-******************************************************************************/
+﻿/* Start Header ************************************************************************/
+/*!
+\file		Vector3.cpp
+\title		Memory's Flame
+\author		Edwin Lee Zirui, edwinzirui.lee, 2301299 (100%)
+\par		edwinzirui.lee@digipen.edu
+\date		07 September 2024
+\brief		Vector3 class and class operation defination
+
+			All content � 2024 DigiPen Institute of Technology Singapore. All rights reserved.
+*/
+/* End Header **************************************************************************/
 
 
 #include "Vector2.h"
@@ -16,43 +17,70 @@ Brief		 : Vector3 class and class operation defination
 #include "Matrix3x3.h"
 
 namespace ToastBox {
+	/*!*****************************************************************************
+	  \brief
+	   Default constructor for Vector3
+	*******************************************************************************/
 	Vec3::Vec3() :x{ 0 }, y{ 0 }, z{ 0 } {}
+
+	/*!*****************************************************************************
+	  \brief
+	   Parameterized constructor for Vector3
+
+	  \param _x
+	   X component of the vector
+
+	  \param _y
+	   Y component of the vector
+
+	  \param _z
+	   Z component of the vector
+	*******************************************************************************/
 	Vec3::Vec3(const float x, const float y, const float z) : x{ x }, y{ y }, z{ z } {}
 
-	/*
-	Function: invert
-	Desc: Invert the vector
-	Example: -a = -1 *a = <-x,-y,-z>
-	*/
-	void Vec3::invert() {
+	/*!*****************************************************************************
+	  \brief
+	   Invert the vector
+	*******************************************************************************/
+	void Vec3::Invert() {
 		x = -x;
 		y = -y;
 		z = -z;
 	}
 
-	/*
-	Function magnitude
-	Desc: Return magnitude of the vector
-	*/
-	float Vec3::magnitude() const {
+	/*!*****************************************************************************
+	  \brief
+	   Calculate the magnitude of the vector
+
+	  \return
+	   Magnitude of the vector
+	*******************************************************************************/
+	float Vec3::Magnitude() const {
 		return real_sqrt(x * x + y * y + z * z);
 	}
 
-	/*
-	Function: magnitudeSquared
-	Desc: Return squared magnitude of the vector
-	*/
-	float Vec3::magnitudeSquared() const {
+	/*!*****************************************************************************
+	  \brief
+	   Calculate the squared magnitude of the vector
+
+	  \return
+	   Squared magnitude of the vector
+	*******************************************************************************/
+	float Vec3::Magnitude_Squared() const {
 		return x * x + y * y + z * z;
 	}
 
-	/*
-	Function: normalize
-	Param: pResult -> For returning of normalized vector
-	Param: pVec0   -> The vector to be normalized
-	Desc: Normlized vector
-	*/
-	void Vec3::normalize(Vec3& pResult, const Vec3& pVec0) {
+	/*!*****************************************************************************
+	  \brief
+	   Normalize a given vector
+
+	  \param _result
+	   Vector to store the normalized result
+
+	  \param _vec0
+	   Vector to be normalized
+	*******************************************************************************/
+	void Vec3::Normalize(Vec3& pResult, const Vec3& pVec0) {
 		Vec3 result;
 		result.x = pVec0.x * pVec0.x;
 		result.y = pVec0.y * pVec0.y;
@@ -66,31 +94,40 @@ namespace ToastBox {
 
 	}
 
-	/*
-	Function: operator *=
-	Param: value
-	Desc: Multiplies vector by param <value>
-	*/
+	/*!*****************************************************************************
+	  \brief
+	   Multiply the vector by a scalar
+
+	  \param _value
+	   Scalar value to multiply by
+	*******************************************************************************/
 	void Vec3::operator*= (const float value) {
 		x *= value;
 		y *= value;
 		z *= value;
 	}
 
-	/*
-	Function: operator *
-	Param: value
-	Desc: Return a  a copy of the vector scaled to param <value>. Not to be confused by dot product
-	*/
+	/*!*****************************************************************************
+	 \brief
+	  Multiply the vector by a scalar and return the result
+
+	 \param _value
+	  Scalar value to multiply by
+
+	 \return
+	  Resulting vector after multiplication
+   *******************************************************************************/
 	Vec3 Vec3::operator*(const float value) const {
 		return Vec3(x * value, y * value, z * value);
 	}
 
-	/*
-	Function: operator/=
-	Param: pVec0
-	Desc: Divides the vector by param pVec0 component level
-	*/
+	/*!*****************************************************************************
+	 \brief
+	  Divide this vector by another vector component-wise
+
+	 \param _vec0
+	  Vector to divide by
+   *******************************************************************************/
 	void Vec3::operator/=(const Vec3& pVec0) {
 		if (pVec0.x == 0) {
 			x = 0;
@@ -114,11 +151,16 @@ namespace ToastBox {
 		}
 	}
 
-	/*
-	Function: operator/
-	Param: value
-	Desc: Return a new vector by dividing current vector with param<value> component level division
-	Example: c = a / b = <a.x, a.y> / <b.x, b.y > = <a.x / b.x, a.y / b.y*/
+	/*!*****************************************************************************
+	  \brief
+	   Divide this vector by a scalar and return the result
+
+	  \param _value
+	   Scalar value to divide by
+
+	  \return
+	   Resulting vector after division
+	*******************************************************************************/
 	Vec3 Vec3::operator/(const float value) const {
 		if (value == 0) {
 			return Vec3(0, 0, 0);
@@ -128,157 +170,231 @@ namespace ToastBox {
 		}
 	}
 
-	/*
-	Function: operator+=
-	Param: pVec0
-	Desc: Return a new vector by adding current vector with param<pVec0> vector
-	Example: c = a + b = <a.x, a.y, a.z> + <b.x, b.y, b.z> = <a.x + b.x, a.y + b.y, a.z + b.z>*/
+	/*!*****************************************************************************
+	  \brief
+	   Add another vector to this vector
+
+	  \param _vec0
+	   Vector to add
+	*******************************************************************************/
 	void Vec3::operator+=(const Vec3& pVec0) {
 		x += pVec0.x;
 		y += pVec0.y;
 		z += pVec0.z;
 	}
 
-	/*
-	Function: operator+
-	Param: pVec0
-	Desc: Return a new vector by adding current vector with param<pVec0> vector
-	Example: c = a + b = <a.x, a.y, a.z> + <b.x, b.y, b.z> = <a.x + b.x, a.y + b.y, a.z + b.z>*/
+	/*!*****************************************************************************
+	  \brief
+	   Add two vectors and return the result
+
+	  \param _vec0
+	   Vector to add
+
+	  \return
+	   Resulting vector after addition
+	*******************************************************************************/
 	Vec3 Vec3::operator+(const Vec3& pVec0) const {
 		return Vec3(x + pVec0.x, y + pVec0.y, z + pVec0.z);
 	}
 
-	/*
-	Function: addition
-	Param: vec1, vec2
-	Desc: Return addition result of 2 vector vec1 and vec2*/
-	Vec3 Vec3::addition(const Vec3& vec1, const Vec3& vec2) {
+	/*!*****************************************************************************
+	  \brief
+	   Add two vectors and return the result
+
+	  \param _vec1
+	   First vector to add
+
+	  \param _vec2
+	   Second vector to add
+
+	  \return
+	   Resulting vector after addition
+	*******************************************************************************/
+	Vec3 Vec3::Addition(const Vec3& vec1, const Vec3& vec2) {
 		return Vec3(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
 	}
 
-	/*
-	Function: operator-=
-	Param: pVec0
-	Desc: Return a new vector by subracting current vector with param<pVec0> vector
-	Example: c = a - b = <a.x, a.y,a.z> - <b.x, b.y,b.z> = <a.x - b.x, a.y - b.y, a.z - b.z>*/
+	/*!*****************************************************************************
+	  \brief
+	   Subtract another vector from this vector
+
+	  \param _vec0
+	   Vector to subtract
+	*******************************************************************************/
 	void Vec3::operator-=(const Vec3& pVec0) {
 		x -= pVec0.x;
 		y -= pVec0.y;
 		z -= pVec0.z;
 	}
 
-	/*
-	Function: operator-
-	Param: pVec0
-	Desc: Return a new vector by subracting current vector with param<pVec0> vector
-	Example: c = a - b = <a.x, a.y,a.z> - <b.x, b.y,b.z> = <a.x - b.x, a.y - b.y, a.z - b.z>*/
+	/*!*****************************************************************************
+	  \brief
+	   Subtract two vectors and return the result
+
+	  \param _vec0
+	   Vector to subtract
+
+	  \return
+	   Resulting vector after subtraction
+	*******************************************************************************/
 	Vec3 Vec3::operator-(const Vec3& pVec0) const {
 		return Vec3(x - pVec0.x, y - pVec0.y, z - pVec0.z);
 	}
 
-	/*
-	Function: subtraction
-	Param: vec1, vec2
-	Desc: Return the result of subraction between vec1 and vec2 as in vec1 - vec2 = result
-	*/
-	Vec3 Vec3::subtraction(const Vec3& vec1, const Vec3& vec2) {
+	/*!*****************************************************************************
+	  \brief
+	   Subtract two vectors and return the result
+
+	  \param _vec1
+	   Vector to subtract from
+
+	  \param _vec2
+	   Vector to subtract
+
+	  \return
+	   Resulting vector after subtraction
+	*******************************************************************************/
+	Vec3 Vec3::Subtraction(const Vec3& vec1, const Vec3& vec2) {
 		return Vec3(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
 	}
 
-	/*
-	Function: scaledVectorAdding
-	Param: value
-	Desc: Return a copy of the vector scaled to param <value>
-	Example: a + kb = <a.x, a.y,a.z> + k<b.x, b.y,b.z > = <a.x + kb.x, a.y + kb.y,a.z + kb.z>
-	Notes: This function can be used update position of point as in
-	position = (velocity * time) + acceleration * time^2 * 0.5
-	can be calulated with
-	object.position = scaledVectorAdding(object.velocity,time) and
-	object.position = scaledVectorAdding(object.acceleration,time*time*0.5)
-	*/
-	void Vec3::scaledVectorAdding(const Vec3& pVec0, float k) {
+	/*!*****************************************************************************
+	  \brief
+	   Add a scaled version of another vector to this vector
+
+	  \param _vec0
+	   Vector to scale and add
+
+	  \param _k
+	   Scalar value to multiply _vec0 by before adding
+	*******************************************************************************/
+	void Vec3::Scaled_Vector_Adding(const Vec3& pVec0, float k) {
 		x += pVec0.x * k;
 		y += pVec0.y * k;
 		z += pVec0.z * k;
 	}
 
-	/*
-	Function: productComponent
-	Param: pVec0
-	Desc: Multiples individual component of a vector by param<pVec0> corresponding component value
-	Example: c = a productComponent b = <a.x, a.y, a.z> productComponent <b.x, b.y, b.z> = <a.x * b.x, a.y * b.y, a.z * b.z> */
-	Vec3 Vec3::productComponent(const Vec3& pVec0) const {
+	/*!*****************************************************************************
+	  \brief
+	   Compute the component-wise product of two vectors
+
+	  \param _vec0
+	   Vector to multiply with
+
+	  \return
+	   Resulting vector after component-wise multiplication
+	*******************************************************************************/
+	Vec3 Vec3::Product_Component(const Vec3& pVec0) const {
 		return Vec3(x * pVec0.x, y * pVec0.y, z * pVec0.z);
 	}
 
-	/*
-	Function: productComponentUpdate
-	Param: pVec0
-	Desc: Multiples individual component of a vector by param<pVec0> corresponding component value
-	Example: c = a productComponent b = <a.x, a.y, a.z> productComponent <b.x, b.y, b.z> = <a.x * b.x, a.y * b.y, a.z * b.z> */
-	void Vec3::productComponentUpdate(const Vec3& pVec0) {
+	/*!*****************************************************************************
+	  \brief
+	   Update this vector with the component-wise product of another vector
+
+	  \param _vec0
+	   Vector to multiply with
+	*******************************************************************************/
+	void Vec3::Product_Component_Update(const Vec3& pVec0) {
 		x *= pVec0.x;
 		y *= pVec0.y;
 		z *= pVec0.z;
 	}
 
-	/*
-	Function: productScalar
-	Param: pVec0
-	Desc: Dot-Product Operation
-	Return scalar product of current vector with another vector <pVec3>.
-	Example: a * b = <a.x, a.y, a.z> * <b.x, b.y, b.z> = a.x * b.x + a.y * b.y + a.z * b.z*/
-	float Vec3::productScalar(const Vec3& pVec0) const {
+	/*!*****************************************************************************
+	  \brief
+	   Compute the dot product with another vector
+
+	  \param _vec0
+	   Vector to compute dot product with
+
+	  \return
+	   Resulting scalar after dot product
+	*******************************************************************************/
+	float Vec3::Product_Scalar(const Vec3& pVec0) const {
 		return x * pVec0.x + y * pVec0.y + z * pVec0.z;
 	}
 
-	/*
-	Function: operator*
-	Param: pVec0
-	Desc: Dot-Product Operation
-	Return scalar product of current vector with another vector <pVec3>.
-	Example: a * b = <a.x, a.y, a.z> * <b.x, b.y, b.z> = a.x * b.x + a.y * b.y + a.z * b.z */
+	/*!*****************************************************************************
+	  \brief
+	   Compute the dot product with another vector (operator overload)
+
+	  \param _vec0
+	   Vector to compute dot product with
+
+	  \return
+	   Resulting scalar after dot product
+	*******************************************************************************/
 	float Vec3::operator*(const Vec3& pVec0) const {
 		return x * pVec0.x + y * pVec0.y + z * pVec0.z;
 	}
 
-	/*
-	Function: productCross
-	Param: pVec0
-	Desc: Return cross product vector */
-	Vec3 Vec3::productCross(const Vec3& pVec0) const {
+	/*!*****************************************************************************
+	  \brief
+	   Compute the cross product with another vector
+
+	  \param _vec0
+	   Vector to compute cross product with
+
+	  \return
+	   Resulting vector after cross product
+	*******************************************************************************/
+	Vec3 Vec3::Cross_Product(const Vec3& pVec0) const {
 		return Vec3(y * pVec0.z - z * pVec0.y,
 			z * pVec0.x - x * pVec0.z,
 			x * pVec0.y - y * pVec0.x);
 	}
 
-	/*
-	Function: operator%=
-	Param: pVec0
-	Desc: Return cross product vector */
+	/*!*****************************************************************************
+	  \brief
+	   Compute the cross product with another vector and store the result in this vector
+
+	  \param _vec0
+	   Vector to compute cross product with
+	*******************************************************************************/
 	void Vec3::operator%=(const Vec3& pVec0) {
-		productCross(pVec0);
+		Cross_Product(pVec0);
 	}
 
-	/*
-	Function: operator%
-	Param: pVec0
-	Desc: Return cross product vector */
+	/*!*****************************************************************************
+	 \brief
+	  Compute the cross product with another vector and return the result
+
+	 \param _vec0
+	  Vector to compute cross product with
+
+	 \return
+	  Resulting vector after cross product
+   *******************************************************************************/
 	Vec3 Vec3::operator%(const Vec3& pVec0) const {
 		return Vec3(y * pVec0.z - z * pVec0.y,
 			z * pVec0.x - x * pVec0.z,
 			x * pVec0.y - y * pVec0.x);
 	}
 
-	/*
-	Function: clear
-	Desc: Reset the points*/
-	void Vec3::clear() {
+	/*!*****************************************************************************
+	  \brief
+	   Reset the vector to zero
+	*******************************************************************************/
+	void Vec3::Clear() {
 		x = 0;
 		y = 0;
 		z = 0;
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Multiply a matrix by a vector
+
+	  \param _mtx0
+	   Matrix to multiply with
+
+	  \param _vec0
+	   Vector to multiply by
+
+	  \return
+	   Resulting vector after multiplication
+	*******************************************************************************/
 	Vec3 operator*(const Matrix3x3& pMtx0, const Vec3& pVec0)
 	{
 		Matrix3x3 pTrans{};
@@ -293,6 +409,19 @@ namespace ToastBox {
 		
 	}
 
+	/*!*****************************************************************************
+	  \brief
+	   Check if two vectors are equal
+
+	  \param _lhs
+	   Left-hand side vector
+
+	  \param _rhs
+	   Right-hand side vector
+
+	  \return
+	   True if vectors are equal, false otherwise
+	*******************************************************************************/
 	bool operator==(Vec3 const& lhs, Vec3 const& rhs) {
 		return (lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z);
 	}
