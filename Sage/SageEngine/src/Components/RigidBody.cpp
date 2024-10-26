817bdee14bb80d34dc3b298bd1773c8ba20f3961
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2024-10-26 20:47:27 +0800

    Update

--- Sage/SageEngine/src/Components/RigidBody.cpp
+++ Sage/SageEngine/src/Components/RigidBody.cpp
@@ -41,7 +41,7 @@ void RigidBody::Update()
 
 void RigidBody::Exit() {}
 
-ComponentType RigidBody::Get_Component_Type() { return PHYSICS; }
+ComponentType RigidBody::Get_Component_Type() { return RIGIDBODY; }
 
 void RigidBody::Set_Gravity_Disable(bool _is_static)
 {
